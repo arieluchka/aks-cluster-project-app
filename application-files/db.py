ff89eec5f808b297977d078b51e0017c4a1e71fe
Ariel Agranovich <ariel.agra@gmail.com>
2023-08-31 14:45:03 +0300

    added some stuff to db.py

--- application-files/db.py
+++ application-files/db.py
@@ -5,7 +5,7 @@ DB_IP = '40.118.48.151'
 
 try:
     connection = psycopg2.connect(host=os.getenv("DB_IP") if os.getenv("DB_IP") != None else DB_IP, port="5432",
-                                  database='postgres', user='postgres', password='test123')
+                                  database='Users', user='postgres', password='test123')
 except:
     print("I am unable to connect to the database")
 
@@ -14,16 +14,16 @@ except:
 
 # connection = psycopg2.connect(host=os.getenv("DB_IP") if os.getenv("DB_IP") != None else DB_IP, port="5432", database='test_erp', user='postgres', password='deeznuts12345_')
 
-# cursor = connection.cursor()
-# cursor.execute("""SELECT * FROM clients""")
-#
-# print(cursor)
-# for query in cursor:
-#     print(str(query))
+cursor = connection.cursor()
+cursor.execute("""SELECT * FROM user_info2 WHERE id=1""")
+
+print(cursor)
+for query in cursor:
+    print(str(query))
 
 
     #"INSERT INTO clients (id, first_name, last_name, role) VALUES (2, 'ariel', 'agranovich', 'admin')"
-    # """SELECT * FROM clients"""
+    # """SELECT * FROM clients WHERE id=1"""
 
 
 #import psycopg2
@@ -32,13 +32,13 @@ except:
 #
 # cursor = connection.cursor()
 #
-# try:
-#     cursor.execute("INSERT INTO clients (id, first_name, last_name, role) VALUES (2, 'ariel', 'agranovich', 'admin')")
-#     connection.commit()  # Commit the transaction
-#     print("Record inserted successfully.")
-# except psycopg2.Error as e:
-#     connection.rollback()  # Roll back the transaction in case of an error
-#     print("Error:", e)
-# finally:
-#     cursor.close()
-#     connection.close()
+try:
+    cursor.execute("INSERT INTO user_info2 VALUES (1, {b, a, n}, 'nuts')")
+    connection.commit()  # Commit the transaction
+    print("Record inserted successfully.")
+except psycopg2.Error as e:
+    connection.rollback()  # Roll back the transaction in case of an error
+    print("Error:", e)
+finally:
+    cursor.close()
+    connection.close()
